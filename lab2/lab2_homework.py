1f4cda9292a400c5f4dea42c24ac0ff25f0b22c5
Razvan Urs <razvan.urs4991@yahoo.com>
2017-04-08 12:20:59 +0000

    Change to python3

---
+++ lab2/lab2_homework.py
@@ -0,0 +1,19 @@
+#!python3
+# Exercise 1: A Good First Program
+# Exercise 2: Comments and Pound Characters
+# Exercise 3: Numbers and Math
+# Exercise 4: Variables and Names
+# Exercise 5: More Variables and Printing
+# Exercise 6: Strings and Text
+# Exercise 7: More Printing
+# Exercise 8: Printing, Printing
+# Exercise 9: Printing, Printing, Printing
+# Exercise 10: What Was That?
+# Exercise 11: Asking Questions
+print('How old are you?', end=' ')
+age = input()
+print(f"You are {age} years old")
+# Exercise 12: Prompting People
+age1 = input('How old are you? ')
+print(f"You are {age1} years old")
+# Exercise 18: Names, Variables, Code, Functions
